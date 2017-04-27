1b11b4b15fd34df047be3f7a5ddb678951d499c6
Mr. Jinesh Subhash Mehta <885716@tcsgegdc.com>
2017-04-27 13:00:24 +0530

    - ui router added

--- app/app.config.js
+++ app/app.config.js
@@ -1,10 +1,32 @@
 angular.module('myApp')
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html'
-  });
+.config(($locationProvider, $stateProvider, $urlRouterProvider) => {
+    "ngInject";
 
-  $routeProvider.when('/view2', {
-      templateUrl: 'view2/view2.html'
-    });
-}]);
+    // Define our app routing, we will keep our layout inside the app component
+    // The layout route will be abstract and it will hold all of our app views
+    $stateProvider
+        .state('view1', {
+            url: '/view1',
+            template: 'view1/view1.html'
+        })
+
+        // Dashboard page to contain our goats list page
+
+        .state('view2', {
+            url: '/view2',
+            template: 'view2/view2.html'
+        })
+
+    $urlRouterProvider.otherwise('/view1');
+});
+
+//
+// .config(['$routeProvider', function($routeProvider) {
+//   $routeProvider.when('/view1', {
+//     templateUrl: 'view1/view1.html'
+//   });
+//
+//   $routeProvider.when('/view2', {
+//       templateUrl: 'view2/view2.html'
+//     });
+// }]);
